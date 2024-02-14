5ff1a25ab8be3a6a39ec7357a414384b41c14c8d
JesusGracia <100613686+JOHNR3ACHER@users.noreply.github.com>
2024-02-14 19:14:38 +0000

    inital commit

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,56 @@
 
-
-
-
-mod implement;
-
 fn main(){
+    use std::time::{SystemTime, Duration};
+    use chrono::{DateTime, Utc, TimeZone};
 
-    let name = "Not provided";
-    let course = "Not Provided";
+    enum BookStatus {
+        Available,
+        CheckedOut(SystemTime), // Due date
+        InRepair(String), // Notes on the repair
+    }
+    
+    struct Book {
+        title: String,
+        status: BookStatus,
+    }
+    
 
-    println!("Assignment");
-    println!("{}",name);
-    println!("{}",course);
+    fn display_book_status(book: &Book) {
+        match &book.status {
+            BookStatus::Available => println!("{} is available for borrowing.", book.title),
+            BookStatus::CheckedOut(due_date) => {
+                // Convert SystemTime to DateTime<Utc>
+                let datetime: DateTime<Utc> = due_date.clone().into();
+                // Format the DateTime<Utc> to a string
+                let formatted_date = datetime.format("%Y-%m-%d %H:%M:%S").to_string();
+                println!("{} is checked out. Due date: {}", book.title, formatted_date);
+            },
+            BookStatus::InRepair(notes) => println!("{} is in repair. Notes: {}", book.title, notes),
+        }
+    }
+
+    
+    let due_date = SystemTime::now() + Duration::from_secs(60 * 60 * 24 * 14); // 14 days from now
+
+    let book = Book {
+        title: String::from("Rust Programming"),
+        status: BookStatus::CheckedOut(due_date),
+    };
+
+    display_book_status(&book);
+    
+
+    }
 
 
+fn reading_from_console(){
+    use std::io;
+
+    println!("What's your favorite city?");
+
+    let mut city = String::new();
+    io::stdin().read_line(&mut city).expect("Failed to read line");
+
+    println!("Your favorite city is: {}", city.trim());
 
 }
