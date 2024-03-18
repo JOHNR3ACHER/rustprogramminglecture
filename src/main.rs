c4e0de87fc8ffc136b3b0a5f8da15f9cc387d005
JesusGracia <100613686+JOHNR3ACHER@users.noreply.github.com>
2024-03-18 18:43:23 +0000

    3/18 Lecture

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,57 @@
-
-use std::fs::{self, File};
-use std::io::{self, ErrorKind, Read};
-
-fn read_username_from_file() -> Result<String, io::Error> {
-    let f = File::open("username.txt");
-    let mut f = match f {
-        Ok(file) => file,
-        Err(e) => return Err(e),
-    };
-
-    println!("My file object contains, fle descriptor {:?}", f);
-
-    let mut s = String::new();
-    match f.read_to_string(&mut s) {
-        Ok(bytes_read) => {
-            println!("{}", bytes_read);
-            Ok(s)
-        },
-        Err(e) => Err(e),
-    }
-
-   
-}
-
-fn read_username_from_file_2ver() -> Result<String, io::Error> {
-    let mut f = File::open("username.txt")?;
-    let mut s = String::new();
-    f.read_to_string(&mut s)?;
-    Ok(s)
-}
-
-fn read_username_from_file_3ver() -> Result<String, io::Error> {
-    let mut s = String::new();
-    File::open("username.txt")?.read_to_string(&mut s)?;
-    Ok(s)
-}
-
-fn read_username_from_file_4ver() -> Result<String, io::Error> {
-    fs::read_to_string("username.txt")
-}
-
-fn main() {
-   let name = read_username_from_file().unwrap();
-   let name = read_username_from_file_4ver().unwrap();
-   println!("No error detected, no crash!, {}" , name);
+#[allow(unused_variables)]
+fn turbofish() {
     
+    #[derive(Debug)]
+    struct Pet<T> {
+        cats: T,
+        dogs: T,
+    }
+    
+    
+    // impl Pet<i32> {
+    //     // notice new() is a static method
+    //     fn new() -> Self {
+    //         Pet {
+    //             cats: 5,
+    //             dogs: 10,
+    //         }
+    //     }
+    // }
+
+    // so here a couple problems, by naive assumption
+    // we could continue like, but it defeats the whole purpose of generics
+    // what to do?
+
+    // impl Pet<i64> {
+    //     // notice new() is a static method
+    //     fn new() -> Self {
+    //         Pet {
+    //             cats: 5,
+    //             dogs: 10,
+    //         }
+    //     }
+    // }
+
+    // let pets = Pet::new();
+    // println!("{:?}",pets);
+
+    impl<T> Pet<T> {
+        // notice new() is a static method
+        fn new(a:T,b:T) -> Self {
+            Pet {
+                cats: a,
+                dogs: b,
+            }
+        }
+    }
+    
+    let pets = Pet::<i64>::new(5_i64,10_i64);
+    println!("Cats: {}, Dogs: {}",pets.cats,pets.dogs);
+
+    let pets = Pet::<String>::new("Million".into(),"Billion".into());
+    println!("Cats: {}, Dogs: {}",pets.cats,pets.dogs);
+    
+}
+fn main() {
+    turbofish();
 }
